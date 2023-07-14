a3617b7abd21836f6ed90f3a28faa1bacd727582
İbrahim <ibrahmgvnc@gmail.com>
2023-07-14 21:47:18 +0300

    second commit

--- src/components/InterviewAccord.jsx
+++ src/components/InterviewAccord.jsx
@@ -5,14 +5,15 @@ const InterviewAccord = ({ id, question, answer }) => {
   const [click, setClick] = useState(false);
 
   return (
-    <div>
-      <h1>
-        {id}. {question}
-      </h1>
-      <div onClick={() => setClick(!click)}>
-        <p> {click ? arrowup : arrowdown} </p>
+    <div className="container">
+      <div>
+        <h1>
+          {id}. {question}
+        </h1>
+        <div onClick={() => setClick(!click)}>
+          <p> {click ? arrowup : arrowdown} </p>
+        </div>
       </div>
-
       {click && (
         <div>
           <div onClick={() => setClick(false)}></div>
